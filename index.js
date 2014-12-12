cae4c4d28f658f9fa343e4c85a4d8374f79a2b59
jonschlinkert <dev@sellside.com>
2014-12-12 12:15:58 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+/*!
+ * helper-markdown <https://github.com/jonschlinkert/helper-markdown>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var fs = require('fs');
+var hljs = require('highlight.js');
+var Remarkable = require('remarkable');
+var extend = require('extend-shallow');
+
+/**
+ * Expose `md` helper
+ */
+
+module.exports = function md(name, opts) {
+  opts = extend({sep: '\n', cwd: process.cwd()}, opts);
+  extend(opts, opts.hash);
+
+  // Support for Assemble, Verb and Template
+  if (this && this.app && this.app.views) {
+    extend(opts, this.options.remarkable);
+
+    try {
+      var template = this.app.findPartial(name);
+      var str = template.render(opts);
+      return markdown(opts).render(str);
+    } catch (err) {
+      return err;
+    }
+
+  // Support for handlebars, lo-dash, or any other engine
+  } else {
+    extend(opts, opts.data && opts.data.root);
+    try {
+      return read(name, opts, opts.render);
+    } catch(err) {
+      return err;
+    }
+  }
+  return '';
+};
+
+/**
+ * Utility function for reading files
+ *
+ * @param {String} `filepath`
+ * @param {String} `opts`
+ * @param {Function} `fn` Optionally pass a render / compile function on the options
+ * @return {String}
+ * @api private
+ */
+
+function read(fp, opts, fn) {
+  var str = fs.readFileSync(fp, 'utf8');
+  if (typeof fn === 'function') {
+    str = fn(str)(opts);
+  }
+  return markdown(opts).render(str);
+}
+
+/**
+ * Shared settings for remarkable
+ *
+ * @param {Object} `options`
+ * @return {Object}
+ * @api private
+ */
+
+function markdown(options) {
+  return new Remarkable(extend({
+    breaks: false,
+    html: true,
+    langPrefix: 'lang-',
+    linkify: true,
+    typographer: false,
+    xhtmlOut: false,
+    highlight: function highlight(code, lang) {
+      try {
+        try {
+          return hljs.highlight(lang, code).value;
+        } catch (err) {
+          if (!/Unknown language/i.test(err.message)) {
+            throw err;
+          }
+          return hljs.highlightAuto(code).value;
+        }
+      } catch (err) {
+        return code;
+      }
+    }
+  }, options));
+}
---
+++ test/test.js
@@ -0,0 +1,72 @@
+/*!
+* helper-md <https://github.com/jonschlinkert/helper-md>
+*
+* Copyright (c) 2014 Jon Schlinkert, contributors.
+* Licensed under the MIT License
+*/
+
+'use strict';
+
+var should = require('should');
+var handlebars = require('handlebars');
+var _ = require('lodash');
+var md = require('..');
+
+var Template = require('template');
+var template;
+
+
+describe('md helper', function () {
+  beforeEach(function() {
+    template = new Template();
+    template.helper('md', md);
+
+    template.create('include', {isPartial: true});
+    template.include('one', {content: '# heading <%= name %>', name: 'one'});
+    template.partial('two', {content: '# heading <%= name %>', name: 'two'});
+
+    template.page('home.md', {
+      content: '<%= md("one") %>'
+    });
+  });
+
+  it('should render markdown on the `content` property for the specified template:', function (cb) {
+    template.render('home.md', function(err, content) {
+      content.should.equal('<h1>heading one</h1>\n');
+      cb();
+    });
+  });
+
+  it('should support rendering markdown from a file:', function () {
+    md('test/fixtures/a.md').should.equal('<h1>AAA</h1>\n<blockquote>\n<p>this is aaa</p>\n</blockquote>\n');
+  });
+});
+
+describe('handlebars:', function () {
+  it('should support rendering markdown from a file:', function () {
+    handlebars.registerHelper('md', md);
+    handlebars.compile('{{{md "test/fixtures/a.md"}}}')().should.equal('<h1>AAA</h1>\n<blockquote>\n<p>this is aaa</p>\n</blockquote>\n');
+  });
+
+  it('should use the `render` function passed on the locals to render templates in partials :', function () {
+    handlebars.registerHelper('md', md);
+    var locals = {name: 'CCC', render: handlebars.compile};
+    handlebars.compile('{{{md "test/fixtures/c.md"}}}')(locals).should.equal('<h1>CCC</h1>\n<p>This is CCC</p>\n');
+  });
+});
+
+describe('lodash:', function () {
+  it('should work as a lodash mixin:', function () {
+    _.mixin({md: md});
+    _.template('<%= _.md("test/fixtures/a.md") %>', {}).should.equal('<h1>AAA</h1>\n<blockquote>\n<p>this is aaa</p>\n</blockquote>\n');
+  });
+
+  it('should work when passed to lodash on the locals:', function () {
+    _.template('<%= _.md("test/fixtures/a.md") %>', {md: md}).should.equal('<h1>AAA</h1>\n<blockquote>\n<p>this is aaa</p>\n</blockquote>\n');
+  });
+
+  it('should work as a lodash import:', function () {
+    var settings = {imports: {md: md}};
+    _.template('<%= _.md("test/fixtures/a.md") %>', {}, settings).should.equal('<h1>AAA</h1>\n<blockquote>\n<p>this is aaa</p>\n</blockquote>\n');
+  });
+});
